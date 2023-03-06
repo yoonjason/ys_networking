f0c2837a8d39ab1acbeada01a27deafe24e1e921
Bradley.yoon <bradley.yoon@kakaocorp.com>
2023-03-06 15:34:33 +0900

    update: shared 상수 public으로 변경

--- Sources/ys_networking/ys_networking.swift
+++ Sources/ys_networking/ys_networking.swift
@@ -16,7 +16,7 @@ enum NetworkError: Error {
 
 public class NetworkManager {
 
-    let shared = NetworkManager()
+    public let shared = NetworkManager()
 
     let session = URLSession.shared
     
